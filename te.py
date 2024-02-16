03b0d7354d51fa2c4065198b01b05b6e2b5f7c95
username29800 <134399459+username29800@users.noreply.github.com>
2024-02-16 18:37:09 +0900

    Update te.py

--- te.py
+++ te.py
@@ -11,67 +11,69 @@ def ed(x):
   else:
     return 0
 while run:
-  if 1: #input
-    ui=input('> ')
-  if ed('a'):
-    pc=str(pc)+str(input())
-  if ed('l'):
-    print(pc+'|'+lc)
-  if ed('c'):
-    ec=pc+lc
-    nc=input()
-    pc=ec[:int(nc)]
-    lc=ec[int(nc):]
-  if ed('x'):
-    pc=pc[:-int(input())]
-  if ed('d') and ll!=[]:
-    ll[int(input())-1]=[]
-  if ed('o'):
-    ln=int(input())-1
-    lpc=ll[:ln]
-    llc=ll[ln:]
-    lpc.append('')
-    lpc[-1]=pc+lc
-    ll=lpc+llc
-    pc=''
-    lc=''
-  if ed('la'):
-    ll.append('')
-    ll[-1]=pc+lc
-    pc=''
-    lc=''
-  if ed('y'):
-    cr=ll[int(input())-1]
-  if ed('n'):
-    pc=ll[int(input())-1]
-    lc=''
-  if ed('p'):
-    pc=pc+cr
-  if ed('ls'):
-    for i in ll:
-      print(i)
-  if ed('quit'):
-    run=0
-  if ed('cl'):
-    lb=[]
-    for i in ll:
-      if i!=[]:
-        lb.append(i)
-    ll=lb
-  if ed('of'):
-    of=open(str(input()),'r')
-    ll=of.readlines()
-    for i in ll:
-      if i[-1]=='\n':
-        i=i[:-1]
-    of.close()
-  if ed('wf'):
-    op=''
-    for i in ll:
-      op=op+str(i)+'\n'
-    op=op[:-1]
-    wf=open(str(input()),'w')
-    wf.write(op)
-    wf.close()
-  if ed('ecl'):
-    ll=[]
+  try:
+    if 1: #input
+      ui=input('> ')
+    if ed('a'):
+      pc=str(pc)+str(input())
+    if ed('l'):
+      print(pc+'|'+lc)
+    if ed('c'):
+      ec=pc+lc
+      nc=input()
+      pc=ec[:int(nc)]
+      lc=ec[int(nc):]
+    if ed('x'):
+      pc=pc[:-int(input())]
+    if ed('d') and ll!=[]:
+      ll[int(input())-1]=[]
+    if ed('o'):
+      ln=int(input())-1
+      lpc=ll[:ln]
+      llc=ll[ln:]
+      lpc.append('')
+      lpc[-1]=pc+lc
+      ll=lpc+llc
+      pc=''
+      lc=''
+    if ed('la'):
+      ll.append('')
+      ll[-1]=pc+lc
+      pc=''
+      lc=''
+    if ed('y'):
+      cr=ll[int(input())-1]
+    if ed('n'):
+      pc=ll[int(input())-1]
+      lc=''
+    if ed('p'):
+      pc=pc+cr
+    if ed('ls'):
+      for i in ll:
+        print(i)
+    if ed('quit'):
+      run=0
+    if ed('cl'):
+      lb=[]
+      for i in ll:
+        if i!=[]:
+          lb.append(i)
+      ll=lb
+    if ed('of'):
+      of=open(str(input()),'r')
+      ll=of.readlines()
+      for i in ll:
+        i=i.rstrip()
+      of.close()
+    if ed('wf'):
+      op=''
+      for i in ll:
+        op=op+str(i)+'\n'
+      op=op[:-1]
+      wf=open(str(input()),'w')
+      wf.write(op)
+      wf.close()
+    if ed('ecl'):
+      ll=[]
+  except:
+    pass
