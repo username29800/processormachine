2acdd6bf09b895318042f447fda1d9559bf9def0
username29800 <134399459+username29800@users.noreply.github.com>
2024-02-16 10:17:00 +0900

    Create te.py

---
+++ te.py
@@ -0,0 +1,60 @@
+kwd0=''
+pc=''
+lc=''
+ec=''
+cr=''
+ll=[]
+run=1
+def ed(x):
+  if str(ui)==str(x):
+    return 1
+  else:
+    return 0
+while run:
+  if 1: #input
+    ui=input('> ')
+  if ed('a'):
+    pc=str(pc)+str(input())
+  if ed('l'):
+    print(pc+'|'+lc)
+  if ed('c'):
+    ec=pc+lc
+    nc=input()
+    pc=ec[:int(nc)]
+    lc=ec[int(nc):]
+  if ed('x'):
+    pc=pc[:-int(input())]
+  if ed('d') and ll!=[]:
+    ll[int(input())-1]=[]
+  if ed('o'):
+    ln=int(input())-1
+    lpc=ll[:ln]
+    llc=ll[ln:]
+    lpc.append('')
+    lpc[-1]=pc+lc
+    ll=lpc+llc
+    pc=''
+    lc=''
+  if ed('la'):
+    ll.append('')
+    ll[-1]=pc+lc
+    pc=''
+    lc=''
+  if ed('y'):
+    cr=ll[int(input())-1]
+  if ed('n'):
+    pc=ll[int(input())-1]
+    lc=''
+  if ed('p'):
+    pc=pc+cr
+  if ed('ls'):
+    for i in ll:
+      print(i)
+  if ed('quit'):
+    run=0
+  if ed('cl'):
+    lb=[]
+    for i in ll:
+      if i!=[]:
+        lb.append(i)
+    ll=lb
