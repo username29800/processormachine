0bc0e754ee6ae22f597cfc8a11d38b2b47c4b363
username29800 <134399459+username29800@users.noreply.github.com>
2024-02-27 22:34:49 +0900

    Update te.py

--- te.py
+++ te.py
@@ -243,7 +243,7 @@ ol - Append to Line then Append to Document''')
             pc=pc+lc[:i+len(pa[0])]
             lc=lc[i+len(pa[0]):]
         pc=pc[:-len(pa[0])]+pa[1]
-        lc=pc+lc
-        pc=''
+        lc=pc[-1]+lc
+        pc=pc[:-1]
   except Exception as xp:
     print(xp)
